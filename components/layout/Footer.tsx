c3b02972c5cb95f1aba7d30454cd922fef556715
Aleksander Kostopoulos <aleksander.kostopoulos@gmail.com>
2025-10-15 21:21:13 +0200

    designet frontsiden

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,9 +1,30 @@
 import styles from "./Footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>
+      <figure className={styles.absolute1}>
+        <Image
+          src="/ClimateImage/blue.png"
+          alt=""
+          width={600}
+          height={600}
+          priority={true}
+        />
+      </figure>
+
+      <figure className={styles.absolute2}>
+        <Image
+          src="/ClimateImage/blue.png"
+          alt=""
+          width={600}
+          height={600}
+          priority={true}
+        />
+      </figure>
+
+      <div className={styles.footerText}>
         <p>{new Date().getFullYear()} Klimakampen</p>
         <small> © All rights reserved Aleksander Kostopoulos</small>
       </div>
--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,16 +1,56 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./HeroSection.module.css";
 
 export default function HeroSection() {
   return (
     <section aria-labelledby="hero-title" className={styles.hero}>
-      <h1 id="hero-title">Klimakampen starter med oss</h1>
-      <p>
+      <Image
+        className={styles.absolute5}
+        aria-hidden="true"
+        src="/ClimateImage/coldThree.png"
+        alt=""
+        width={300}
+        height={900}
+      />
+
+      <Image
+        className={styles.absolute6}
+        aria-hidden="true"
+        src="/ClimateImage/warmthThreeFour.png"
+        alt=""
+        width={300}
+        height={900}
+      />
+
+      <Image
+        className={styles.absolute7}
+        aria-hidden="true"
+        src="/ClimateImage/coldThree.png"
+        alt=""
+        width={300}
+        height={900}
+      />
+
+      <Image
+        className={styles.absolute8}
+        aria-hidden="true"
+        src="/ClimateImage/warmthThreeFour.png"
+        alt=""
+        width={300}
+        height={900}
+      />
+
+      <h1 id="hero-title" className={styles.heroTitle}>
+        Klimakampen starter med oss
+      </h1>
+      <p className={styles.text}>
         Vi står midt i en avgjørende tid. Sammen kan vi skape endring, redusere
         utslipp og bygge en bærekraftig fremtid – for oss selv og kommende
         generasjoner.
       </p>
-      <Link href="/klimatiltak" className="link">
+
+      <Link href="/klimatiltak" className={styles.heroLink}>
         Bli med i kampen
       </Link>
     </section>
